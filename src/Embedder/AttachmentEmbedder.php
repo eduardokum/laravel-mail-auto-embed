83349a6a35560edb1c95e31205e2a848d73196ef
Eduardo Gusmao <eduguscontra3@hotmail.com>
2020-04-13 14:47:22 -0300

    embed from remote

--- src/Embedder/AttachmentEmbedder.php
+++ src/Embedder/AttachmentEmbedder.php
@@ -2,6 +2,7 @@
 
 namespace Eduardokum\LaravelMailAutoEmbed\Embedder;
 
+use Illuminate\Support\Str;
 use Swift_Image;
 use Swift_Message;
 use Swift_EmbeddedFile;
@@ -31,12 +32,42 @@ class AttachmentEmbedder extends Embedder
         $filePath = str_replace(url('/'), public_path('/'), $url);
 
         if (!file_exists($filePath)) {
+            if ($embeddedFromRemoteUrl = $this->fromRemoteUrl($filePath)) {
+                return $embeddedFromRemoteUrl;
+            }
             return $url;
         }
 
         return $this->embed(
             Swift_Image::fromPath($filePath)
         );
+    }
+
+    /**
+     * @param  string  $url
+     */
+    public function fromRemoteUrl($url)
+    {
+        if (filter_var($url, FILTER_VALIDATE_URL)) {
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            $raw = curl_exec($ch);
+            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+            curl_close($ch);
+
+            if ($httpcode == 200) {
+                return $this->embed(
+                    new Swift_Image($raw, Str::random(10), $contentType)
+                );
+            }
+        }
+
+        return null;
     }
 
     /**
